8e6915b0fcfbc8a6d8e09635e8451a55d1974b88
abcdefg <abc@def.ghi>
2019-09-10 16:45:03 +0300

    pronunciations of arabic words

--- client/src/components/arabicListWords/index.js
+++ client/src/components/arabicListWords/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import forvoApi from 'forvo';
 import './style.css'
 
 class arabicListWords extends Component {
@@ -13,17 +14,32 @@ class arabicListWords extends Component {
     })
   }
 
+  getPronunciation=()=>{
+    // const forvo = forvoApi({ key: '2997f047b9a545330bb9f3776fce7ee0'});
+    // const start = async () => {
+    //   const wordPronunciations = await forvo.pronouncedWordsSearch({ search: word, language: 'ar' })
+    //   alert(wordPronunciations)
+    // }
+    //
+    // start().catch(err => console.log(err.stack));
+    axios.get(`https://apifree.forvo.com/key/2997f047b9a545330bb9f3776fce7ee0/format/json/action/pronounced-words-search/search/hello/language/en`).then(({ data }) => {
+      console.log('result',data)
+    })
+
+  }
+
   render() {
     return (
       <div>
         <h1> List Of Arabic Words </h1>
         <React.Fragment>
+        <button onClick={()=>this.getPronunciation()}/>
           {this.state.arabicList.map(({ english, arabic, pronunciation }) => (
             <ul>
-              <li className="list">
-                {english}
+              <li className="word-item">
+                {english}  :
                 {arabic}
-                {pronunciation}
+                '{pronunciation}'
               </li>
             </ul>
           ))}
